41f7e754ce29d1fe75025670dfdb270603e0ed69
Giada <giadaunger@Giadas-MacBook-Air.local>
2023-09-27 11:25:29 +0200

    Tried to import test_function1 in main.py and test2.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from say_hello import say_hello
+from test1 import test_function1
 
 say_hello()
+
+test_function1()
 
 if __name__ == "__main__":
     print("The program has started")
--- test/test2.py
+++ test/test2.py
@@ -1,2 +1,6 @@
+from test1 import test_function1
+
 def test_function2():
     print("This is test function 2")
+
+test_function1()
